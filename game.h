08b1d891bd56e99c166f5c820d70ef0bffbaa004
Mayank Sharma <mayank202020@rediffmail.com>
2017-03-29 14:48:13 +0530

    Create game.h

---
+++ game.h
@@ -0,0 +1,15 @@
+      #include<bits/stdc++.h>
+      using namespace std;
+
+      const char human ='X';
+      const char ab = 'O';
+
+      // Combining the no of player use enum union
+      enum player {HUMAN,com};
+
+      struct Move
+      {
+        //Cordinates
+        int x;
+        int y;
+      }; 
